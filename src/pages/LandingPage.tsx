e27e0376d69771695cf882610e9d1298b8f6437d
austhiiin <jausthin7@gmail.com>
2026-03-14 00:59:42 +0800

    feat: about section

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,15 @@
 
 import Navbar from "../components/Navbar"
 import HeroSection from "../components/HeroSection"
+import AboutSection from "../components/AboutSection"
 
 function LandingPage() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <AboutSection />
+      
     </>
   )
 }
